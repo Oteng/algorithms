dd5f7f55b821df3f4d6a82837bb7f042b074938f
Kwaku Oteng <otengkwaku@gmail.com>
2022-10-16 01:28:34 +0000

    refactor logic into function

--- c:c++/insertion_sort.c
+++ c:c++/insertion_sort.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+int insertion_sort(int *arr, int length){
+    for(int i = 1; i < length; i++){
+            int key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > key){
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    return 0;
+}
+
 int main(int arg, char** argv){
 
     int unsorted_arr[] = {5,2,4,6,1,3};
@@ -7,19 +20,11 @@ int main(int arg, char** argv){
 
     printf("Length of array = %d \n", arr_length);
 
-    for(int i = 1; i < arr_length; i++){
-        int key = unsorted_arr[i];
-        int j = i - 1;
-        while(j >= 0 && unsorted_arr[j] > key){
-            unsorted_arr[j + 1] = unsorted_arr[j];
-            j = j - 1;
-        }
-        unsorted_arr[j + 1] = key;
-    }
+    insertion_sort(&unsorted_arr, arr_length);
 
     for(int i=0; i < arr_length; i++) {
-        printf("%d, ", unsorted_arr[i]);
-    }
+            printf("%d, ", unsorted_arr[i]);
+        }
     return 0;
 }
 
