34c5aa00d7633f88587cf86ba8e40954568dec95
Kwaku Oteng <otengkwaku@gmail.com>
2022-10-16 17:59:04 +0000

    Refactored the two functions into one

--- python/insertion_sort.py
+++ python/insertion_sort.py
@@ -1,5 +1,5 @@
 # Sort in increasing order
-def insertion_sort(arr):
+def insertion_sort_asc(arr):
     for i in range(1, len(arr)):
         value = arr[i]
         j = i - 1
@@ -21,7 +21,21 @@ def insertion_sort_dec(arr):
     return arr
 
 
+# combining the two function into one
+def insertion_sort(arr, direction='asc'):
+    for i in range(1, len(arr)):
+        value = arr[i]
+        j = i - 1
+        while j >= 0 and ((arr[j] > value) == (direction == 'asc')):
+            arr[j + 1] = arr[j]
+            j = j - 1
+        arr[j + 1] = value
+    return arr
+
 if __name__ == '__main__':
 # Tests
-    print('Array sorted in ascending order: ', insertion_sort([5,2,4,6,1,3]))
+    print('Array sorted in ascending order: ', insertion_sort_asc([5,2,4,6,1,3]))
     print('Array sorted in descending order: ', insertion_sort_dec([5,2,4,6,1,3]))
+
+    print('Array sorted in ascending order: ', insertion_sort([5,2,4,6,1,3]))
+    print('Array sorted in ascending order: ', insertion_sort([5,2,4,6,1,3], 'desc'))
