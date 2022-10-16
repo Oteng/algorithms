be143e2854a8e03e360d1745d05f6bb34337cbae
Kwaku Oteng <otengkwaku@gmail.com>
2022-10-16 01:32:33 +0000

    refactor insertion sort into function

--- javasort/InsertionSort.java
+++ javasort/InsertionSort.java
@@ -5,8 +5,12 @@ import java.util.Arrays;
 class InsertionSort{
     public static void main(String[] args){
 
-        int arr[] = {5,2,4,6,1,3};
+        int arr[] = {5,2,10,4,6,1,3};
 
+        System.out.println(Arrays.toString(insertionSort(arr)));
+    }
+
+    public static int[] insertionSort(int[] arr){
         for(int i = 1; i < arr.length; i++){
             int value = arr[i];
             int j = i - 1;
@@ -16,7 +20,6 @@ class InsertionSort{
             }
             arr[j + 1] = value;
         }
-
-        System.out.println(Arrays.toString(arr));
+        return arr;
     }
 }
