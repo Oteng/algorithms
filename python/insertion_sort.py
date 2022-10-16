f9d8aa75d81ab189ede1453b46571f1b3dee5cb9
Kwaku Oteng <otengkwaku@gmail.com>
2022-10-16 17:40:50 +0000

    added python insertion sort

---
+++ python/insertion_sort.py
@@ -0,0 +1,14 @@
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        value = arr[i]
+        j = i - 1;
+        while j >= 0 and arr[j] > value:
+            arr[j + 1] = arr[j];
+            j = j - 1
+        arr[j + 1] = value
+    return arr
+
+if __name__ == '__main__':
+
+    print(insertion_sort([5,2,4,6,1,3]))
